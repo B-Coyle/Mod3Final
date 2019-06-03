dcbde6f00e8296d69dfe3ced41925d0a15aa5878
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-03 11:31:29 -0600

    Add minor css styling

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from '../Card/index';
+import './CardContainer.css';
 
 export const CardContainer = ({presidents}) => {
     const cards = presidents.map(president => <Card president={president} key={president.name} />)
