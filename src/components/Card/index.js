e4422ced499103b17e81fb48990a82c823338749
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-03 10:33:30 -0600

    Add card information

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,2 +1,14 @@
-import React from 'react'
+import React from "react";
 
+export const Card = ({ president }) => {
+  return (
+  <article className="card">
+    <p>{president.number}</p>
+    <p>{president}</p>
+    <p>{president.birth_year}</p>
+    <p>{president.death_year}</p>
+    <p>{president.took_office}</p>
+    <p>{president.left_office}</p>
+    <p>{president.party}</p>
+  </article>)
+};
