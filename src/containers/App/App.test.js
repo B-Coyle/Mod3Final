21a1f15e528d4be208667f882850ca53ba4ce370
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-03 11:21:40 -0600

    Add passing test for MSTP

---
+++ src/containers/App/App.test.js
@@ -0,0 +1,33 @@
+import {mapStateToProps, mapDispatchToProps} from './index';
+
+describe('App', () => {
+    const mockProps = {
+        isLoading: false,
+        hasErrored: '',
+        presidents: [{president: 'Washington'}]
+    }
+
+    describe('mapStateToProps', () => {
+        it('should return a props object with isLoading, hasErrored, and an array of presidents', () => {
+            const mockState= {
+                ...mockProps,
+                assholes: []
+            }
+            const expected = mockProps
+            const mappedProps = mapStateToProps(mockState)
+            expect(mappedProps).toEqual(expected);
+        })
+    })
+
+    describe('mapDispatchToProps', () => {
+        it('should dispatch the fetchPresidents thunk', () => {
+            const mockDispatch = jest.fn()
+            const mappedProps = mapDispatchToProps(mockDispatch);
+            mappedProps.fetchPresidents()
+            expect(mockDispatch).toHaveBeenCalled()
+        })
+    })
+
+
+
+})
