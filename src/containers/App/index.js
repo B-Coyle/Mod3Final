26fbb88a6c378ecc91db4472c357356d5e2d7baf
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-03 10:19:00 -0600

    Update App with MSTP and MDTP

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,23 +1,34 @@
-import React, {Component} from 'react';
-import './App.css';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { fetchPresidents } from "../../thunks/fetchPresidents";
 
-export default class App extends Component {
-    constructor(){
-        super()
-        this.state={
+export class App extends Component {
+  constructor() {
+    super();
+  }
 
-        }
-    }
+  componentDidMount() {}
 
-    componentDidMount(){
-    }
-
-    render(){
-        return(
-            <h1>Presidents and Assholes</h1>
-        )
-    }
+  render() {
+    const { hasErrored, isLoading, presidents } = this.props;
+    return (
+      <main>
+        <h1>Presidents and Assholes</h1>
+        {hasErrored && <h2>Error loading information about presidents</h2>}
+      </main>
+    );
+  }
 }
 
+export const mapStateToProps = (state) => ({
+    isLoading: state.isLoading,
+    hasErrored: state.hasErrored,
+    presidents: state.presidents
+})
 
+export const mapDispatchToProps = (dispatch) => ({
+    fetchPresidents: () => dispatch(fetchPresidents()); 
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
