2b7b24117b099d0ae42c23c22db79f3d0028d5aa
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-03 10:26:47 -0600

    Add cardcontainer

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,2 +1,12 @@
 import React from 'react'
+import { Card } from '../Card/index';
 
+export const CardContainer = ({presidents}) => {
+    const cards = presidents.map(president => <Card president={president} key={president.name} />)
+
+        return(
+            <section className="container">
+                {cards}
+            </section>
+        )
+}
