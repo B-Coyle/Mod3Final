4a5193d6204dc04865085df6df65b07f3fc10c3c
Bridgett Coyle <bacoyle0409@gmail.com>
2019-06-03 11:36:00 -0600

    Add clarification to the card for information

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,12 +4,12 @@ import './Card.css'
 export const Card = ({ president }) => {
   return (
   <article className="card">
-    <p>{president.number}</p>
-    <p>{president.president}</p>
-    <p>{president.birth_year}</p>
-    <p>{president.death_year}</p>
-    <p>{president.took_office}</p>
-    <p>{president.left_office}</p>
-    <p>{president.party}</p>
+    <p>President Number: {president.number}</p>
+    <p>Name: {president.president}</p>
+    <p>Year Born: {president.birth_year}</p>
+    <p>Year Died: {president.death_year}</p>
+    <p>Year took office: {president.took_office}</p>
+    <p>Year left office: {president.left_office}</p>
+    <p>Affiliation: {president.party}</p>
   </article>)
 };
